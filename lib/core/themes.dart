41439314570b4b8aea8d4c09aeaabfe9ef762678
Emre Evien <evirenemre02@gmail.com>
2025-01-30 23:07:03 +0300

    temalandır ,renkler ve fontlar oluşturuldu

--- lib/core/themes.dart
+++ lib/core/themes.dart
@@ -1,1 +1,106 @@
 //burada temalandırma + renkler + ... yazicaz
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class ThemeProvider with ChangeNotifier {
+  bool _isDark = false;
+  bool get isDark => _isDark;
+
+  void toggleTheme() {
+    _isDark = !_isDark;
+    notifyListeners();
+  }
+}
+
+const colors = {
+  "primary": Color.fromARGB(255, 230, 57, 71),
+  "onPrimary": Color.fromARGB(255, 255, 255, 255),
+  "secondary": Color.fromARGB(255, 255, 255, 255),
+  "onSecondary": Color.fromARGB(255, 14, 39, 66),
+  "surface": Color.fromARGB(255, 255, 255, 255),
+  "onSurface": Color.fromARGB(255, 14, 39, 66),
+  "success": Color.fromARGB(255, 54, 238, 0),
+  "error": Color.fromARGB(255, 194, 0, 0),
+  "onError": Color.fromARGB(255, 255, 255, 255),
+};
+
+const darkColors = {
+  "primary": Color.fromARGB(255, 14, 39, 66),
+  "onPrimary": Color.fromARGB(255, 255, 255, 255),
+  "secondary": Color.fromARGB(255, 14, 39, 66),
+  "onSecondary": Color.fromARGB(255, 255, 255, 255),
+  "surface": Color.fromARGB(255, 23, 33, 43),
+  "onSurface": Color.fromARGB(255, 245, 245, 245),
+  "success": Color.fromARGB(255, 54, 238, 0),
+  "error": Color.fromARGB(255, 194, 0, 0),
+  "onError": Color.fromARGB(255, 255, 255, 255),
+};
+
+final lightTheme = ThemeData(
+  brightness: Brightness.light,
+  colorScheme: ColorScheme(
+    brightness: Brightness.light,
+    primary: colors["primary"]!,
+    onPrimary: colors["onPrimary"]!,
+    secondary: colors["secondary"]!,
+    onSecondary: colors["onSecondary"]!,
+    error: colors["error"]!,
+    onError: colors["onError"]!,
+    surface: colors["surface"]!,
+    onSurface: colors["onSurface"]!,
+  ),
+  textTheme: TextTheme(
+    bodySmall: GoogleFonts.roboto(
+        // fontSize: 32,
+        ),
+    bodyMedium: GoogleFonts.roboto(),
+    bodyLarge: GoogleFonts.roboto(),
+    labelSmall: GoogleFonts.roboto(),
+    labelMedium: GoogleFonts.roboto(),
+    labelLarge: GoogleFonts.roboto(),
+    titleSmall: GoogleFonts.roboto(),
+    titleMedium: GoogleFonts.roboto(),
+    titleLarge: GoogleFonts.roboto(),
+    headlineSmall: GoogleFonts.aBeeZee(),
+    headlineMedium: GoogleFonts.aBeeZee(),
+    headlineLarge: GoogleFonts.aBeeZee(),
+    displaySmall: GoogleFonts.abrilFatface(),
+    displayMedium: GoogleFonts.abrilFatface(),
+    displayLarge: GoogleFonts.abrilFatface(),
+  ),
+);
+
+final darkTheme = ThemeData(
+  brightness: Brightness.dark,
+  colorScheme: ColorScheme.dark(
+    brightness: Brightness.dark,
+    primary: darkColors["primary"]!,
+    onPrimary: darkColors["onPrimary"]!,
+    secondary: darkColors["secondary"]!,
+    onSecondary: darkColors["onSecondary"]!,
+    error: darkColors["error"]!,
+    onError: darkColors["onError"]!,
+    surface: darkColors["surface"]!,
+    onSurface: darkColors["onSurface"]!,
+  ),
+  textTheme: TextTheme(
+    bodySmall: GoogleFonts.roboto(
+        // fontSize: 30,
+        ),
+    bodyMedium: GoogleFonts.roboto(),
+    bodyLarge: GoogleFonts.roboto(),
+    labelSmall: GoogleFonts.roboto(),
+    labelMedium: GoogleFonts.roboto(),
+    labelLarge: GoogleFonts.roboto(),
+    titleSmall: GoogleFonts.roboto(),
+    titleMedium: GoogleFonts.roboto(),
+    titleLarge: GoogleFonts.roboto(),
+    headlineSmall: GoogleFonts.aBeeZee(),
+    headlineMedium: GoogleFonts.aBeeZee(),
+    headlineLarge: GoogleFonts.aBeeZee(),
+    displaySmall: GoogleFonts.abrilFatface(),
+    displayMedium: GoogleFonts.abrilFatface(),
+    displayLarge: GoogleFonts.abrilFatface(),
+  ),
+);
