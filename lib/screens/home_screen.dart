da647ffde22ed67aada90728c02dddca642d5cda
Emre Evien <evirenemre02@gmail.com>
2025-01-30 23:05:39 +0300

    ana sayfa düzenlendi

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,191 +1,177 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_app/wigdets/bottom_menu.dart';
 import 'package:go_router/go_router.dart';
-
-
-
-import '../wigdets/bottom_menu.dart';
+import '../core/themes.dart';
+import '../widgets/bottom_menu.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
-        // AppBar
-        appBar: AppBar(
-          backgroundColor: const Color.fromARGB(255, 230, 57, 71),
-          title: const Text(
-            'ROAD+',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 30,
-            ),
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        title: Text(
+          'ROAD+',
+          style: Theme.of(context)
+              .textTheme
+              .headlineLarge!
+              .copyWith(color: Theme.of(context).colorScheme.onPrimary),
+        ),
+        actions: [
+          
+          IconButton(
+            color: Theme.of(context).colorScheme.onPrimary,
+            icon: const Icon(CupertinoIcons.bell, size: 30),
+            onPressed: () {
+              context.go("/notifications");
+            },
           ),
-          actions: [
-            IconButton(
-              color: Colors.white,
-              icon: const Icon(
-                CupertinoIcons.bell,
-                size: 30,
+        ],
+      ),
+      // Drawer (Yan Menü)
+      drawer: Drawer(
+        backgroundColor: Theme.of(context).colorScheme.surface,
+        elevation: 0,
+        child: Column(
+          children: [
+            // Drawer Header
+            Container(
+              height: 200,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    CupertinoIcons.person_circle,
+                    size: 80,
+                    color: Theme.of(context).colorScheme.onSecondary,
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'EMRE EVİREN',
+                    style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                          color: Theme.of(context).colorScheme.onSecondary,
+                        ),
+                  ),
+                ],
               ),
-              onPressed: () {
-                context.go("/notifications");
+            ),
+            // Menü öğeleri
+            ListTile(
+              leading: Icon(
+                CupertinoIcons.home,
+                color: Theme.of(context).colorScheme.onSecondary,
+              ),
+              title: Text(
+                'Ana Sayfa',
+                style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                      color: Theme.of(context).colorScheme.onSecondary,
+                    ),
+              ),
+              onTap: () {
+                context.go("/home");
+              },
+            ),
+            ListTile(
+              leading: Icon(
+                CupertinoIcons.arrow_counterclockwise,
+                color: Theme.of(context).colorScheme.onSecondary,
+              ),
+              title: Text(
+                'Geçmiş',
+                style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                      color: Theme.of(context).colorScheme.onSecondary,
+                    ),
+              ),
+              onTap: () {
+                context.push("/history");
+              },
+            ),
+            ListTile(
+              leading: Icon(
+                CupertinoIcons.ticket,
+                color: Theme.of(context).colorScheme.onSecondary,
+              ),
+              title: Text(
+                'Biletlerim',
+                style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                      color: Theme.of(context).colorScheme.onSecondary,
+                    ),
+              ),
+              onTap: () {
+                context.go("/ticket");
+              },
+            ),
+            ListTile(
+              leading: Icon(
+                CupertinoIcons.car,
+                color: Theme.of(context).colorScheme.onSecondary,
+              ),
+              title: Text(
+                'Araçlarım',
+                style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                      color: Theme.of(context).colorScheme.onSecondary,
+                    ),
+              ),
+              onTap: () {
+                context.go("/car");
+              },
+            ),
+            ListTile(
+              leading: Icon(
+                CupertinoIcons.settings,
+                color: Theme.of(context).colorScheme.onSecondary,
+              ),
+              title: Text(
+                'Ayarlar',
+                style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                      color: Theme.of(context).colorScheme.onSecondary,
+                    ),
+              ),
+              onTap: () {
+                context.push("/settings");
               },
             ),
           ],
         ),
+      ),
 
-        // Drawer (Yan Menü)
-        drawer: Drawer(
-          backgroundColor: Colors.white,
-          elevation: 0,
-          child: Column(
-            children: [
-              // Drawer Header
-              SizedBox(
-                height: 200,
-                child: const Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(
-                      CupertinoIcons.person_circle,
-                      size: 80,
-                      color: Color.fromARGB(255, 29, 53, 87),
-                    ),
-                    SizedBox(height: 10),
-                    Text(
-                      'EMRE EVİREN',
-                      style: TextStyle(
-                        color: Color.fromARGB(255, 29, 53, 87),
-                        fontSize: 18,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              // Menü öğeleri
-              ListTile(
-                leading: const Icon(
-                  CupertinoIcons.home,
-                  color: Color.fromARGB(255, 29, 53, 87),
-                ),
-                title: const Text('Ana Sayfa'),
-                onTap: () {
-                  context.go("/home");
-                },
-              ),
-              ListTile(
-                leading: const Icon(
-                  CupertinoIcons.arrow_counterclockwise,
-                  color: Color.fromARGB(255, 29, 53, 87),
-                ),
-                title: const Text('Geçmiş'),
-                onTap: () {
-                  context.go("/history");
-                },
-              ),
-              ListTile(
-                leading: const Icon(
-                  CupertinoIcons.ticket,
-                  color: Color.fromARGB(255, 29, 53, 87),
-                ),
-                title: const Text('Biletlerim'),
-                onTap: () {
-                  context.go("/ticket");
-                },
-              ),
-              ListTile(
-                leading: const Icon(
-                  CupertinoIcons.car,
-                  color: Color.fromARGB(255, 29, 53, 87),
-                ),
-                title: const Text(
-                  'Araçlarım',
-                ),
-                onTap: () {
-                  context.go("/car");
-                },
-              ),
-              ListTile(
-                leading: const Icon(
-                  CupertinoIcons.settings,
-                  color: Color.fromARGB(255, 29, 53, 87),
-                ),
-                title: const Text('Ayarlar'),
-                onTap: () {
-                  context.go("/settings");
-                },
-              ),
-            ],
-          ),
-        ),
-
-        // Ana içerik
-
-        body: Column(
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Expanded(
-              child: Container(
-                padding: const EdgeInsets.all(0),
-                child: const Text(
-                  'MERHABA EMRE,',
-                  style: TextStyle(fontSize: 30),
-                ),
+            Text(
+              'MERHABA EMRE,',
+              style: Theme.of(context).textTheme.displayMedium!.copyWith(
+                    fontWeight: FontWeight.bold,
+                  ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Seyahatlerinizi kolaylaştıran uygulamaya hoş geldiniz!',
+              style: Theme.of(context).textTheme.bodyLarge,
+            ),
+            SizedBox(height: 20),
+            Card(
+              elevation: 5,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(16),
+                child: Image.asset('assets/images/kampanya.png',
+                    fit: BoxFit.cover),
               ),
             ),
-            Image.asset('assets/images/kampanya.png'),
-
-            Expanded(
-                child: Container(
-                    padding: const EdgeInsets.all(15),
-                    child: const Text(
-                      'KATEGORİLER',
-                      style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: Color.fromARGB(255, 29, 53, 87)),
-                    ))),
-            const Expanded(
-                child: Row(
-              children: [
-
-                Padding(padding: EdgeInsets.all(20)),
-                Icon(
-                  CupertinoIcons.airplane,
-                  color: Color.fromARGB(255, 29, 53, 87),
-                  size: 80,
-                ),
-                Padding(padding: EdgeInsets.all(20)),
-                Icon(
-                  CupertinoIcons.car,
-                  color: Color.fromARGB(255, 29, 53, 87),
-                  size: 80,
-                ),
-                Padding(padding: EdgeInsets.all(20)),
-                Icon(
-                  CupertinoIcons.train_style_one,
-                  color: Color.fromARGB(255, 29, 53, 87),
-                  size: 80,
-                ),
-                Padding(padding: EdgeInsets.all(20)),
-                Icon(
-                  CupertinoIcons.bus,
-                  size: 80,
-                  color: Color.fromARGB(255, 29, 53, 89),
-                ),
-
-
-              ],
-            )),
-            const SizedBox(
-              width: 800,
-            )
           ],
         ),
+      ),
 
-        // Alt navigasyon çubuğu
-        bottomNavigationBar: const BottomMenu());
+      bottomNavigationBar: BottomMenu(),
+    );
   }
 }
+
+
